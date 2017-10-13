d9039579fa88c10ed0dde2c9bee23e061bb0a745
Gregorio Hernandez Caso <gregoriohc@gmail.com>
2017-10-13 21:32:25 +0200

    Improve SlugizeKeys transformer

--- src/Transformers/SlugizeKeys.php
+++ src/Transformers/SlugizeKeys.php
@@ -5,13 +5,23 @@ namespace Ghc\Rosetta\Transformers;
 class SlugizeKeys extends Transformer
 {
     /**
+     * Boot Transformer
+     */
+    protected function boot()
+    {
+        $this->addDefaultConfig([
+            'separator' => '_',
+        ]);
+    }
+
+    /**
      * @param array $data
      * @return array
      */
     public function transform($data)
     {
         return collect($data)->mapWithKeys(function($value, $key) {
-            return [str_slug($key) => !is_array($value) ? $value : $this->transform($value)];
+            return [str_replace('-', $this->config['separator'], str_slug($key)) => !is_array($value) ? $value : $this->transform($value)];
         })->toArray();
     }
 }
