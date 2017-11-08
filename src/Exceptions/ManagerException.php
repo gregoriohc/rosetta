068bf75cb6a02b5a4dca312353f30f4f68dc1c47
Gregorio Hernandez Caso <gregoriohc@gmail.com>
2017-11-08 18:11:32 +0100

    Refactor ManagerException

--- src/Exceptions/ManagerException.php
+++ src/Exceptions/ManagerException.php
@@ -6,4 +6,48 @@ use Exception;
 
 class ManagerException extends Exception
 {
+    /**
+     * @param $class
+     * @return static
+     */
+    public static function undefinedConnector($class)
+    {
+        return new static("Connector class '$class' does not exists");
+    }
+
+    /**
+     * @param $class
+     * @return static
+     */
+    public static function undefinedMessage($class)
+    {
+        return new static("Message class '$class' does not exists");
+    }
+
+    /**
+     * @param $class
+     * @return static
+     */
+    public static function undefinedTransformer($class)
+    {
+        return new static("Transformer class '$class' does not exists");
+    }
+
+    /**
+     * @param $class
+     * @return static
+     */
+    public static function undefinedPipe($class)
+    {
+        return new static("Pipe class '$class' does not exists");
+    }
+
+    /**
+     * @param $class
+     * @return static
+     */
+    public static function undefinedMatcher($class)
+    {
+        return new static("Matcher class '$class' does not exists");
+    }
 }
